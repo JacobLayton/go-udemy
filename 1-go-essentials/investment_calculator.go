dcae62f8c2ad217fa2bd4301ab533b37dad71dc3
JacobLayton <jlayjlay3@gmail.com>
2026-01-25 19:44:44 -0800

    Abstract math functions to separate func with multiple return values

--- 1-go-essentials/investment_calculator.go
+++ 1-go-essentials/investment_calculator.go
@@ -5,6 +5,8 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
 	const inflationRate float64 = 2.5
 	var investmentAmount, years, expectedReturnRate float64
@@ -18,12 +20,19 @@ func main() {
 	fmt.Print("Enter duration (years): ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
-	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
+	// futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	// futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
 	formattedFv := fmt.Sprintf("Future value: %.1f\n", futureValue)
 	formattedRfv := fmt.Sprintf("Future value (inflation adjusted): %.1f\n", futureRealValue)
 
 	// fmt.Printf("Future value: %.0f\nFuture value (inflation adjusted): %.0f\n", futureValue, futureRealValue)
 	fmt.Print(formattedFv, formattedRfv)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	rfv := fv / math.Pow(1 + inflationRate / 100, years)
+	return fv, rfv
 }
