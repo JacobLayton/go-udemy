62ef863b930660dee2076a1c921705fd0e9e28bc
JacobLayton <jlayjlay3@gmail.com>
2026-01-29 20:26:22 -0800

    Add constructor function with error validation and error handling

--- 5-go-structs/structs.go
+++ 5-go-structs/structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -22,18 +23,33 @@ func (u *user) clearUserName() {
 	u.lastName = ""
 }
 
+// Constructor function
+func newUser(firstName, lastName, birthdate string) (*user, error) {
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("all fields are required")
+	}
+
+
+	return &user{
+		firstName: firstName,
+		lastName: lastName,
+		birthdate: birthdate,
+		createdAt: time.Now(),
+	}, nil
+}
+
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	var appUser user
+	var appUser *user
 
-	appUser = user{
-		firstName: userFirstName,
-		lastName: userLastName,
-		birthdate: userBirthdate,
-		createdAt: time.Now(),
+	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+
+	if err != nil {
+		fmt.Println("Error creating user:", err)
+		return
 	}
 
 	appUser.outputUserDetails()
@@ -44,6 +60,6 @@ func main() {
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
