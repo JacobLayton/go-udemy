0dcd43c27e509010cca9802f8d3d74b5c039d828
JacobLayton <jlayjlay3@gmail.com>
2026-01-29 20:37:43 -0800

    Move user struct and methods to different package and import them

--- 5-go-structs/structs.go
+++ 5-go-structs/structs.go
@@ -1,60 +1,28 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+
+	"example.com/structs/user"
 )
-
-type user struct {
-	firstName string
-	lastName  string
-	birthdate string
-	createdAt time.Time
-}
-
-func (u user) outputUserDetails() {
-	fmt.Println(u.firstName, u.lastName, u.birthdate, u.createdAt)
-}
-
-// Since we're trying to mutate the original struct, we need to use a pointer receiver
-func (u *user) clearUserName() {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-// Constructor function
-func newUser(firstName, lastName, birthdate string) (*user, error) {
-	if firstName == "" || lastName == "" || birthdate == "" {
-		return nil, errors.New("all fields are required")
-	}
-
-
-	return &user{
-		firstName: firstName,
-		lastName: lastName,
-		birthdate: birthdate,
-		createdAt: time.Now(),
-	}, nil
-}
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	var appUser *user
+	var appUser *user.User
 
-	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+	appUser, err := user.New(userFirstName, userLastName, userBirthdate)
 
 	if err != nil {
 		fmt.Println("Error creating user:", err)
 		return
 	}
 
-	appUser.outputUserDetails()
-	appUser.clearUserName()
-	appUser.outputUserDetails()
+	appUser.OutputUserDetails()
+	appUser.ClearUserName()
+	appUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {
