bb31573bce6a4ad61defb89303d807d88049692f
JacobLayton <jlayjlay3@gmail.com>
2026-02-06 21:16:29 -0800

    Create map and use the delete keyword, same as regular objects

---
+++ 7-go-data-handling/maps.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"Google": "http://google.com",
+		"Amazon Web Services": "http://aws.com",
+	}
+	fmt.Println(websites)
+	fmt.Println(websites["Amazon Web Services"])
+	websites["Linkedin"] = "http://linkedin.com"
+	fmt.Println(websites)
+
+	delete(websites, "Google")
+	fmt.Println(websites)
+}
