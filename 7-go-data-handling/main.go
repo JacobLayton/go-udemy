ca1647985641257ba04561e5ba4d234a961b7143
JacobLayton <jlayjlay3@gmail.com>
2026-02-07 21:34:54 -0800

    Add type alias to clean code

--- 7-go-data-handling/main.go
+++ 7-go-data-handling/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) output(){
+	fmt.Println(m)
+}
+
 func main() {
 	userNames := make([]string, 2, 5) // length 2, capacity 5
 
@@ -14,12 +20,14 @@ func main() {
 
 	fmt.Println(userNames)
 
-	courseRatings := make(map[string]float64, 3)
+	courseRatings := make(floatMap, 3)
 	courseRatings["go"] = 4.7
 	courseRatings["react"] = 4.8
 	courseRatings["angular"] = 4.6
 
-	fmt.Println(courseRatings)
+	courseRatings.output()
+
+	// fmt.Println(courseRatings)
 	// The point of utilizing the make function is to set the initial memory allocation for the slice or map, which
 	// can improve performance by reducing the need for multiple memory allocations as elements are added. In this
 	// example, we set the initial capacity of the userNames slice to 5, which means it can hold up to 5 elements
