b54e41a0b89d7beaca4fd692980b30453a46ba7b
JacobLayton <jlayjlay3@gmail.com>
2026-02-09 20:10:42 -0800

    Create anonymous function inline

--- 8-go-functions/main.go
+++ 8-go-functions/main.go
@@ -2,29 +2,22 @@ package main
 
 import "fmt"
 
-type transformFn func(int) int
-
 func main() {
-	numbers := []int{1, 2, 3, 4}
-	doubled := transformNumbers(&numbers, double)
-	fmt.Println(doubled)
-	tripled := transformNumbers(&numbers, triple)
-	fmt.Println(tripled)
+	numbers := []int{1, 2, 3}
+
+	transformed := transformNumbers(&numbers, func (number int) int {
+		return number * 2
+	})
+
+	fmt.Println(transformed)
 }
 
-func transformNumbers(numbers *[]int, transform transformFn) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
+
 	for _, val := range *numbers {
 		dNumbers = append(dNumbers, transform(val))
 	}
 
 	return dNumbers
-}
-
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
 }
