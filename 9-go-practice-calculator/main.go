2911f0ff5a35e660fae88e902e3398dedd40a174
JacobLayton <jlayjlay3@gmail.com>
2026-02-16 17:03:17 -0800

    Add sleep to tax calc to simulate slow process, apply goroutine

--- 9-go-practice-calculator/main.go
+++ 9-go-practice-calculator/main.go
@@ -9,17 +9,23 @@ import (
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
+	doneChans := make([]chan bool, len(taxRates))
 
-	for _, taxRate := range taxRates {
+	for index, taxRate := range taxRates {
+		doneChans[index] = make(chan bool)
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate * 100))
 		// cmdm := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		err := priceJob.Process()
+		go priceJob.Process(doneChans[index])
 
-		if err != nil {
-			fmt.Println("Could not process job")
-			fmt.Println(err)
-			return
-		}
+		// if err != nil {
+		// 	fmt.Println("Could not process job")
+		// 	fmt.Println(err)
+		// 	return
+		// }
+	}
+
+	for _, doneChan := range doneChans {
+		<- doneChan
 	}
 }
