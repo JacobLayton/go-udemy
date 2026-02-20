4179b9eead6fadd737a0db744c13589efe887e9d
JacobLayton <jlayjlay3@gmail.com>
2026-02-19 21:48:21 -0800

    Set up sqlite db

--- 11-go-rest-api/main.go
+++ 11-go-rest-api/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"example.com/restapi/db"
 	"example.com/restapi/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
@@ -32,5 +34,6 @@ func createEvent(context *gin.Context) {
 
 	event.ID = 1
 	event.UserID = 1
+	event.Save()
 	context.JSON(http.StatusCreated, gin.H{"message": "Event Created!", "event": event})
 }
