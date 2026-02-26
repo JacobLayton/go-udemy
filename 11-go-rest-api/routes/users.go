ae5beabef4037651a2c1c1a86f0d6fae9eb2adca
JacobLayton <jlayjlay3@gmail.com>
2026-02-25 20:06:34 -0800

    Install golang-jwt, add GenerateToken func, pass token with login response

--- 11-go-rest-api/routes/users.go
+++ 11-go-rest-api/routes/users.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"example.com/restapi/models"
+	"example.com/restapi/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -37,5 +38,12 @@ func login(context *gin.Context) {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "Could not authenticate user"})
 		return
 	}
-	context.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+
+	token, err := utils.GenerateToken(user.Email, user.ID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate user"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful", "token": token})
 }
