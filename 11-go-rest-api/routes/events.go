3db3a69bb8ff817ed431e626592c42a17e624bb7
JacobLayton <jlayjlay3@gmail.com>
2026-02-25 20:43:46 -0800

    Add verify token check, convert create event to protected route

--- 11-go-rest-api/routes/events.go
+++ 11-go-rest-api/routes/events.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"example.com/restapi/models"
+	"example.com/restapi/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -32,8 +33,19 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+	err := utils.VerifyToken(token)
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized"})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse req data"})
